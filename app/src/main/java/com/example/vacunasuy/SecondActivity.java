ffc5d4ab1645607e34f14fc86ae9f7420276df9e
Valentin-VP <valevp2010@hotmail.com>
2021-05-16 20:21:05 -0300

    funcionando, se trae la cedula

--- app/src/main/java/com/example/vacunasuy/SecondActivity.java
+++ app/src/main/java/com/example/vacunasuy/SecondActivity.java
@@ -2,59 +2,84 @@ package com.example.vacunasuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
+
+import com.android.volley.NetworkResponse;
+
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Map;
+import java.net.URL;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 
 public class SecondActivity extends AppCompatActivity {
+    //esta clase se va a crearse cuando se llegue a un http://localhost
 
+    private String token;
+    private TextView cedula;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        System.out.println("Entro a SecondActivity");
+        cedula = (TextView) findViewById(R.id.cedula);
 
         Uri URIdata = getIntent().getData();
-        if(URIdata != null) {
-            System.out.println("llega a la segunda actividad");
+        if (URIdata != null) {
 
 
             String uriString = URIdata.toString();
-            String url = uriString.replaceAll("localhost/", "10.0.2.2:8080/grupo15-services/callback");
-            RequestQueue rq = Volley.newRequestQueue(this);
-            StringRequest or = new StringRequest(Request.Method.GET, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            System.out.println(response);
+            String url = uriString.replaceAll("localhost/", "10.0.2.2:8080/grupo15-services/callback"); //cambio el link para que se direccione a donde quiero
 
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            System.out.println(error.toString());
-                        }
-                    }
-            );
-            rq.add(or);
+            //defino el cliente para hacer el http request
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .followRedirects(false)
+                    .build();
+            //defino el request
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
 
-            // String jasonWebToken = URIdata.getQueryParameter("jwt");
-        }
+            //hago el http request
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) { //si falla
+                    e.printStackTrace();
+                }
 
-
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull okhttp3.Response response) throws IOException { //si obtengo respuesta
+                    final String myResponse = response.header("x-access-token");
+                    System.out.println(myResponse);
+                    token = myResponse;
+                    getCi();
+                }
+            });
 
 
 //        Intent intent = new Intent(this, MainActivity.class);
@@ -63,6 +88,44 @@ public class SecondActivity extends AppCompatActivity {
 
 //        finish();
 
-
+        }else {//si no obtengo el url capturado termino la actividad y vuevlo al main
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
     }
+
+    public void getCi(){
+        String url = "http://10.0.2.2:8080/grupo15-services/rest/mobile/id";
+        OkHttpClient client = new OkHttpClient();
+        //defino el request
+        Request request = new Request.Builder()
+                .addHeader("x-access-token", token)
+                .url(url)
+                .build();
+
+        //hago el http request
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) { //si falla
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull okhttp3.Response response) throws IOException { //si obtengo respuesta
+                if(response.body() != null) {
+                    String ci = Objects.requireNonNull(response.body()).string();
+                    System.out.println(ci);
+                    cedula.setText(ci);
+                }else{
+                    System.out.println("Response.body es null");
+                    //manejar que no existe el usuario
+                }
+            }
+        });
+    }
+
+
 }
+
